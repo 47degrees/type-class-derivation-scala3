865a65b261976213280429b75c6616017543dcd5
Javi Martinez <javier.martinez@47deg.com>
2023-01-18 10:15:46 +0100

    Extract the projection logic from the interpolated string

--- src/main/scala/typeclassderivation/shapeless3/Show.scala
+++ src/main/scala/typeclassderivation/shapeless3/Show.scala
@@ -1,7 +1,6 @@
 package typeclassderivation.shapeless3
 
 import shapeless3.deriving.*
-import typeclassderivation.scala3.Show
 
 trait Show[A]:
   def show(a: A): String
@@ -15,20 +14,21 @@ object Show:
   given Show[String] = identity(_)
 
   def deriveShowProduct[A](using
-      inst: K0.ProductInstances[Show, A],
+      pInst: K0.ProductInstances[Show, A],
       labelling: Labelling[A]
   ): Show[A] =
     (a: A) =>
-        labelling.elemLabels.zipWithIndex
-          .map { (label, index) =>
-            s"$label = ${inst.project(a)(index)([t] => (st: Show[t], pt: t) => st.show(pt))}"
-          }
-          .mkString(s"${labelling.label}(", ", ", ")")
+      labelling.elemLabels.zipWithIndex
+        .map { (label, index) =>
+          val value = pInst.project(a)(index)([t] => (st: Show[t], pt: t) => st.show(pt))
+          s"$label = $value"
+        }
+        .mkString(s"${labelling.label}(", ", ", ")")
 
   def deriveShowSum[A](using
-      inst: K0.CoproductInstances[Show, A]
+      cInst: K0.CoproductInstances[Show, A]
   ): Show[A] =
-    (a: A) => inst.fold(a)([a] => (st: Show[a], a: a) => st.show(a))
+    (a: A) => cInst.fold(a)([a] => (st: Show[a], a: a) => st.show(a))
 
   inline given derived[A](using gen: K0.Generic[A]): Show[A] =
     gen.derive(deriveShowProduct, deriveShowSum)
--- src/test/scala/typeclassderivation/ShowDerivedInstancesSpec.scala
+++ src/test/scala/typeclassderivation/ShowDerivedInstancesSpec.scala
@@ -5,7 +5,7 @@ import typeclassderivation.shapeless3.Show
 
 class ShowDerivedInstancesSpec extends FunSuite {
 
-  final case class Foo(i: Int, s: String, b: Boolean)
+  final case class Foo(x: Int, y: String, z: Boolean)
 
   enum ColorEnum:
     case Red, Green, Blue
@@ -17,27 +17,27 @@ class ShowDerivedInstancesSpec extends FunSuite {
     case object Blue extends Color
 
   enum XEnum:
-    case Y(i: Int, s: String, b: Boolean)
+    case Y(x: Int, y: String, z: Boolean)
     case Z
 
   sealed trait X
   object X:
-    final case class Y(i: Int, s: String, b: Boolean) extends X
+    final case class Y(x: Int, y: String, z: Boolean) extends X
     case object Z extends X
 
   test("Type class instances derived using Shapeless 3") {
 
     import typeclassderivation.shapeless3.Show
 
-    case class Bar(i: Int, s: String, b: Boolean) derives Show
+    case class Bar(x: Int, y: String, z: Boolean) derives Show
 
-    assertEquals(Show[Bar].show(Bar(1, "s", true)), "Bar(i = 1, s = s, b = true)")
-    assertEquals(Show[Foo].show(Foo(1, "s", true)), "Foo(i = 1, s = s, b = true)")
+    assertEquals(Show[Bar].show(Bar(1, "s", true)), "Bar(x = 1, y = s, z = true)")
+    assertEquals(Show[Foo].show(Foo(1, "s", true)), "Foo(x = 1, y = s, z = true)")
     assertEquals(Show[ColorEnum].show(ColorEnum.Blue), "Blue()")
     assertEquals(Show[Color].show(Color.Blue), "Blue()")
     assertEquals(
       Show[XEnum].show(XEnum.Y(1, "s", true)),
-      "Y(i = 1, s = s, b = true)"
+      "Y(x = 1, y = s, z = true)"
     )
     assertEquals(
       Show[XEnum].show(XEnum.Z),
@@ -45,7 +45,7 @@ class ShowDerivedInstancesSpec extends FunSuite {
     )
     assertEquals(
       Show[X].show(X.Y(1, "s", true)),
-      "Y(i = 1, s = s, b = true)"
+      "Y(x = 1, y = s, z = true)"
     )
 
   }
@@ -54,15 +54,15 @@ class ShowDerivedInstancesSpec extends FunSuite {
 
     import typeclassderivation.scala3.Show
 
-    case class Bar(i: Int, s: String, b: Boolean) derives Show
+    case class Bar(x: Int, y: String, z: Boolean) derives Show
 
-    assertEquals(Show[Bar].show(Bar(1, "s", true)), "Bar(i = 1, s = s, b = true)")
-    assertEquals(Show[Foo].show(Foo(1, "s", true)), "Foo(i = 1, s = s, b = true)")
+    assertEquals(Show[Bar].show(Bar(1, "s", true)), "Bar(x = 1, y = s, z = true)")
+    assertEquals(Show[Foo].show(Foo(1, "s", true)), "Foo(x = 1, y = s, z = true)")
     assertEquals(Show[ColorEnum].show(ColorEnum.Blue), "Blue()")
     assertEquals(Show[Color].show(Color.Blue), "Blue()")
     assertEquals(
       Show[XEnum].show(XEnum.Y(1, "s", true)),
-      "Y(i = 1, s = s, b = true)"
+      "Y(x = 1, y = s, z = true)"
     )
     assertEquals(
       Show[XEnum].show(XEnum.Z),
@@ -70,7 +70,7 @@ class ShowDerivedInstancesSpec extends FunSuite {
     )
     assertEquals(
       Show[X].show(X.Y(1, "s", true)),
-      "Y(i = 1, s = s, b = true)"
+      "Y(x = 1, y = s, z = true)"
     )
     assertEquals(
       Show[X].show(X.Z),
